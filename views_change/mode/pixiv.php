8d127dd7036f7498f9ec143a8ae9a3b7cfc92d5d
88nooto <88nooto@gmail.com>
2017-03-02 00:23:26 +0800

    ver0.11.6

---
+++ views_change/mode/pixiv.php
@@ -0,0 +1,26 @@
+<?php
+//$url='http://www.pixiv.net/'; 
+//$ch = curl_init(); 
+//$timeout = 0; 
+//curl_setopt ($ch, CURLOPT_URL, $url); 
+//curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1); //不自动输出内容
+//curl_setopt($ch, CURLOPT_HEADER, 0);//不返回头部信息
+//curl_setopt($ch, CURLOPT_ENCODING, 'gzip'); 
+//curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout); 
+//
+//do {
+//$file_contents1 = curl_exec($ch);//首先执行一次
+//curl_close($ch); 
+//} while (empty($file_contents1));//判断为空时，重复do
+//
+//
+//
+//$rule = '|(<div\sclass=\"wallpaper\"\sstyle=\"background-image\:url\(\'.*\'\)\">)|i';//正则
+//preg_match_all($rule,$file_contents1,$arr,PREG_PATTERN_ORDER);
+//
+//print_r($arr);
+
+
+
+
+?>
